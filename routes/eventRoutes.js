6e527907afa795e0595e103162a4db80672eb2a1
danny8823 <dannyyoo714@gmail.com>
2023-08-13 13:33:40 -0700

    added events routes, get,put,delete

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,2 +1,30 @@
 import express from 'express'
+import Event from '../db/models/eventModels.js'
+const router = express.Router()
 
+router.get('/', async (req,res,next) => {
+    try {
+        const events = await Event.findAll()
+        res.send(events)
+    } catch {
+        res.status(404).send()
+    }
+})
+
+router.post('/', async (req,res,next) => {
+    try {
+        const events = await Event.create(req.body)
+        res.send(events)
+    } catch {
+        res.sendStatus(404)
+    }
+})
+
+router.put('/:id', async(req,res,next) => {
+    try {
+        const events = await Event.update(req.body) 
+        res.send(events)
+    } catch {
+        res.sendStatus(404)
+    }
+})
