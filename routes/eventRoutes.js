1bcd1d131f9c0b9b687ecdf2403e7acbfc7ee23d
danny8823 <dannyyoo714@gmail.com>
2023-08-14 00:53:48 -0700

    installed axios

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -6,17 +6,25 @@ router.get('/', async (req,res,next) => {
     try {
         const events = await Event.findAll()
         res.send(events)
-    } catch {
-        res.status(404).send()
+    } catch(error) {
+        res.status(404).send(error.message)
     }
 })
 
 router.post('/', async (req,res,next) => {
     try {
-        const events = await Event.create(req.body)
+        const {title, description, startDate, endDate, allDay} = req.body
+
+        const events = await Event.create({
+            title: title,
+            description: description,
+            startDate : startDate,
+            endDate : endDate,
+            allDay: allDay
+        })
         res.send(events)
-    } catch {
-        res.sendStatus(404)
+    } catch(error) {
+        throw new Error(error.message)
     }
 })
 
@@ -24,7 +32,9 @@ router.put('/:id', async(req,res,next) => {
     try {
         const events = await Event.update(req.body) 
         res.send(events)
-    } catch {
+    } catch(error) {
         res.sendStatus(404)
     }
 })
+
+export default router
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { db } from './db/db.js'
 import Event from './db/models/eventModels.js'
+import eventRouter from './routes/eventRoutes.js'
 const server = express()
 const port = 3200
 
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
-// server.use('/api',)
+server.use('/api/', eventRouter)
+
 const startServer = async () => {
     try {
         server.listen(port, () => {
@@ -19,6 +21,12 @@ const startServer = async () => {
             description: 'this is a test',
             startDate: new Date(2023,8,1,20,0,0),
             endDate: new Date(2023,8,1,22,30,0)
+        })
+        await Event.create({
+            title: 'big meeting dude',
+            description: 'big meeting with a big dude in a big room about a big subject',
+            startDate: new Date(2023, 8, 2, 23,0 ,0),
+            endDate: new Date(2023, 8, 2, 25, 0 , 0)
         })
         console.log('database connected')
     } catch(error) {
